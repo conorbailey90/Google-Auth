27d177ae9d908e0d1e22d427e343e37e2082b54e
conorbailey90 <conbailey90@gmail.com>
2020-12-14 11:45:25 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,95 @@
+require('dotenv').config()
+
+const express = require('express');
+const app = express();
+const cookieParser = require('cookie-parser')
+
+// Google Auth
+const {OAuth2Client} = require('google-auth-library');
+const CLIENT_ID = '333642631602-h2l1m29lfb5c1d0dta76nvv4so4bjeo4.apps.googleusercontent.com'
+const client = new OAuth2Client(CLIENT_ID);
+
+
+const PORT = 7000;
+
+// Middleware
+
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.static('public'));
+
+app.get('/', (req, res)=>{
+    res.render('index')
+})
+
+app.get('/login', (req,res)=>{
+    res.render('login');
+})
+
+app.post('/login', (req,res)=>{
+    let token = req.body.token;
+
+    async function verify() {
+        const ticket = await client.verifyIdToken({
+            idToken: token,
+            audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+        });
+        const payload = ticket.getPayload();
+        const userid = payload['sub'];
+      }
+      verify()
+      .then(()=>{
+          res.cookie('session-token', token);
+          res.send('success')
+      })
+      .catch(console.error);
+
+})
+
+app.get('/profile', checkAuthenticated, (req, res)=>{
+    let user = req.user;
+    res.render('profile', {user});
+})
+
+app.get('/protectedRoute', checkAuthenticated, (req,res)=>{
+    res.send('This route is protected')
+})
+
+app.get('/logout', (req, res)=>{
+    res.clearCookie('session-token');
+    res.redirect('/login')
+
+})
+
+
+function checkAuthenticated(req, res, next){
+
+    let token = req.cookies['session-token'];
+
+    let user = {};
+    async function verify() {
+        const ticket = await client.verifyIdToken({
+            idToken: token,
+            audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+        });
+        const payload = ticket.getPayload();
+        user.name = payload.name;
+        user.email = payload.email;
+        user.picture = payload.picture;
+      }
+      verify()
+      .then(()=>{
+          req.user = user;
+          next();
+      })
+      .catch(err=>{
+          res.redirect('/login')
+      })
+
+}
+
+
+app.listen(PORT, ()=>{
+    console.log(`Server running on port ${PORT}`);
+})
